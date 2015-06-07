d582bd0090d07d722c1e7dd134b554014117dc5d
dokato <raymon92@gmail.com>
2015-06-07 20:23:00 +0200

    simple preprocessing methods added: filtering/resampling

--- connectivipy/data.py
+++ connectivipy/data.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import scipy.io as si
+import scipy.signal as ss
 from loaders import signalml_loader
 
 class Data(object):
@@ -52,22 +53,35 @@ class Data(object):
                 data = mat_dict[key]
             if data_info=='sml':
                 data, sml = signalml_loader(data_what.split('.')[0])
-                self.smldict = sml # here SignaML data is stored
+                self.smldict = sml # here SignalML data is stored
         else:
             pass 
         return data
             
-    def filter(self,ftype):
-        pass
-
-    def resample(self, fs_new):
+    def filter(self, b, a):
         '''
-        Signal resampling.
+        Filter each channel of data using forward-backward  filter 
+        *filtfilt* from *scipy.signal*.
+
+        Args:
+          *b,a* : np.array 
+            Numerator *b*  / denominator *a* polynomials of the IIR filter.
+        '''
+        
+        self.__data = ss.filtfilt(b,a,self.__data)
+
+    def resample(self, new_fs):
+        '''
+        Signal resampling to new sampling frequency *new_fs* using
+        *resample* function from *scipy.signal* (basing on Fourier method).
         
         Args:
-          *fs_new*  new sampling frequency
+          *new_fs* : int
+            new sampling frequency
         '''
-        pass 
+        new_nr_samples = (len(self.__data[0])*1./self.__fs)*fs_new
+        self.__data = ss.resample(self.__data, new_nr_samples)
+        self.__fs = fs_new
     
     def estimate(self):
         pass
