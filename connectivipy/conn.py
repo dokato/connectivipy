some important fixes in GCI and covariance

connectivipy/conn.py
@@ -71,7 +71,6 @@ def spectrum(acoef, vcoef, fs=1, resolution=100):
         S_z[e] = np.dot(np.dot(H_z[e],vcoef), H_z[e].T.conj())
     return A_z, H_z, S_z
 
-
 def spectrum_inst(acoef, vcoef, fs=1, resolution=None):
     """
     Generating data point from matrix *A* with MVAR coefficients taking
@@ -648,7 +647,7 @@ class GCI(Connect):
             arix = [j for j in xrange(k) if i!=j]
             ar_i, vr_i = Mvar().fit(data[arix,:], order, method)
             for e,c in enumerate(arix):
-                gcval[i,c] = np.log(vrfull[i,i]/vr_i[e,e])
+                gcval[c,i] = np.log(vrfull[i,i]/vr_i[e,e])
         return np.tile(gcval,(2,1,1))
 
 conn_estim_dc = { 'dtf'  : DTF,

connectivipy/mvar/fitting.py
@@ -118,7 +118,7 @@ def ncov(x, y=[], p=0, norm=True):
             zy = y.take(idxs,axis=1, mode='wrap')
             cov[:,:,r] = np.dot(x[:,:N-r],zy[:,:N-r].T)
     if norm:
-        kv = cov/N
+        kv = cov/(N-1)
     else:
         kv = cov 
     if p==0:
@@ -162,16 +162,13 @@ def vieiramorf(y,pmax=1):
         D = cov_func(f[:,k+1:N],b[:,0:N-k-1],norm=False)
         arf[k,:,:] = np.dot(D,np.linalg.inv(peb))
         arb[k,:,:] = np.dot(D.T,np.linalg.inv(pef))
-        
         tmp = f[:,k+1:] - np.dot(b[:,:N-k-1].T,arf[k,:,:].T).T
         b[:,:N-k-1] = b[:,:N-k-1] - np.dot(f[:,k+1:].T,arb[k,:,:].T).T
         f[:,k+1:] = tmp
-
         for i in range(k):
             tmpp = arf[i]-np.dot(arf[k],arb[k-i-1])
             arb[k-i-1,:,:] = arb[k-i-1,:,:] -np.dot(arb[k,:,:],arf[i,:,:])
             arf[i,:,:] = tmpp
-
         peb = cov_func(b[:,:N-k-1],norm=False)
         pef = cov_func(f[:,k+1:],norm=False)
     return arf, pef/N
@@ -213,16 +210,13 @@ def nutallstrand(y,pmax=1):
         D = cov_func(f[:,k+1:N],b[:,0:N-k-1],norm=False)
         arf[k,:,:] = 2*np.dot(D,np.linalg.inv(peb + pef))
         arb[k,:,:] = 2*np.dot(D.T,np.linalg.inv(pef + peb))
-        
         tmp = f[:,k+1:] - np.dot(b[:,:N-k-1].T,arf[k,:,:].T).T
         b[:,:N-k-1] = b[:,:N-k-1] - np.dot(f[:,k+1:].T,arb[k,:,:].T).T
         f[:,k+1:] = tmp
-
         for i in range(k):
             tmpp = arf[i]-np.dot(arf[k],arb[k-i-1])
             arb[k-i-1,:,:] = arb[k-i-1,:,:] -np.dot(arb[k,:,:],arf[i,:,:])
             arf[i,:,:] = tmpp
-
         peb = cov_func(b[:,:N-k-1],norm=False)
         pef = cov_func(f[:,k+1:],norm=False)
     return arf, pef/N
@@ -238,7 +232,7 @@ def yulewalker(y,pmax=1):
           k - nr of channels, n -data points, tr - nr of trials
       *pmax*: int >0
           model order
-    Returns:
+'    Returns:
       *Ar* : np.array
           matrix with parameters matrix (p, k, k) where p - model order,
           k - nr of channels

connectivipy/mvarmodel.py
@@ -99,7 +99,8 @@ class Mvar(object):
         for p in range(p_max):
             (a_coef, v_r) = cls.fit(data, p+1, method)
             crit[p] = np.log(np.linalg.det(v_r))+2.*np.log(np.log(N))*(p+1)*chn**2/N
-        return np.argmin(crit)+1, crit 
+        return np.argmin(crit)+1, crit
+
     @classmethod
     def _order_schwartz(cls, data, p_max=None, method='yw'):
         """
@@ -131,6 +132,7 @@ class Mvar(object):
             (a_coef,v_r) = cls.fit(data,p+1,method)
             crit[p] = np.log(np.linalg.det(v_r))+np.log(N)*(p+1)*chn**2/N
         return np.argmin(crit)+1, crit
+
     @classmethod
     def _order_fpe(cls, data, p_max=None, method='yw'):
         """