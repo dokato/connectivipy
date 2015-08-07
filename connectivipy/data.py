771a01280985f24365aaefc076ed3d6246d26733
dokato <raymon92@gmail.com>
2015-08-07 17:05:34 +0200

    basic version of trans3d data format export

--- connectivipy/data.py
+++ connectivipy/data.py
@@ -29,7 +29,7 @@ class Data(object):
         if self.__data.shape[0]==len(chan_names):
             self.__channames = chan_names
         else:
-            self.__channames = []
+            self.__channames = ["x"+str(i) for i in range(self.__chans)]
         self._parameters = {}
         self._parameters["mvar"] = False
 
@@ -346,9 +346,27 @@ class Data(object):
           *freq_band*=[] : list
             frequency range [from_value, to_value] in Hz.            
         '''
-        
-        with open(filename,'wb') as file:
-            file.write()
+        trdef_str = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, -8, 0, 0, 0, 1]
+        tr_str = [0.0051, 0.0074, -3.4523, 0., -2.7277, 2.1161, 0.0005, 0., 2.1161, 2.7276, 0., -8., 0., 0., 0., 1.]
+        content = ";electrodes = " + " ".join(self.__channames)
+        content += "\r\n;start = -0.500000\r\n"
+        content += ";samplerate = 12\r\n"
+        content += ";transform_default = " + " ".join(trdef_str[:self.__chans]) + "\r\n"
+        content += ";transform = " + " ".join(tr_str[:self.__chans]) + "\r\n"
+        content += "\r\n"
+        # integrate value of estimator in given frequency band
+        freqs = np.linspace(0, int(self.__fs/2), self.__estim.shape[0])
+        if len(freq_band)==0:
+            ind1 = 0
+            ind2 = len(freqs)
+        else:
+            ind1 = np.where(freqs>=freq_band[0])[0][0]
+            ind2 = np.where(freqs>=freq_band[1])[0][0]
+        cnest = np.mean(self.__estim[ind1:ind2,:,:], axis=0)
+        for i in xrange(self.__chans):
+            content+= "  " + "\t".join(cnest[i]) + "\r\n" 
+        with open(filename,'wb') as fl:
+            fl.write(content)
     
     # accessors:
     @property
