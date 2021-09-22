11fef099f5a16ec1113d67f9a4f3789f84c83993
Dominik Krzeminski <raymon92@gmail.com>
2021-09-22 10:47:10 +0100

    fixes in import 2

--- connectivipy/__init__.py
+++ connectivipy/__init__.py
@@ -6,7 +6,5 @@ from .mvarmodel import Mvar
 from .mvar.fitting import mvar_gen, mvar_gen_inst, fitting_algorithms
 from .plot import plot_conn
 
-__all__ = ['data', 'conn']
-
 __version__ = '0.3.7'
 __version_vector__ = (0, 3, 7)
